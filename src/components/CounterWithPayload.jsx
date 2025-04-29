ff339aff9c165d4e0d5cbf120668f5aaccc66b11
mahbub-sakib <mahbub.sakib418@gmail.com>
2025-04-30 00:09:20 +0600

    spreading state in reducer while returning

--- src/components/CounterWithPayload.jsx
+++ src/components/CounterWithPayload.jsx
@@ -1,13 +1,21 @@
 import React, { useReducer } from 'react';
 
-const initialState = { count: 0 };
+const initialState = {
+    count: 0,
+    counter2: 0,
+
+};
 
 function reducer(mystate, action) {
     switch (action.type) {
         case 'increment':
-            return { count: mystate.count + action.payload };
+            return { ...mystate, count: mystate.count + action.payload };
         case 'decrement':
-            return { count: mystate.count - action.payload };
+            return { ...mystate, count: mystate.count - action.payload };
+        case 'increment2':
+            return { ...mystate, counter2: mystate.counter2 + action.payload };
+        case 'decrement2':
+            return { ...mystate, counter2: mystate.counter2 - action.payload };
         default:
             return mystate;
     }
@@ -18,9 +26,16 @@ const CounterWithPayload = () => {
 
     return (
         <div>
-            <h2>Count: {state.count}</h2>
-            <button onClick={() => dispatch({ type: 'decrement', payload: 5 })}>-</button>
-            <button onClick={() => dispatch({ type: 'increment', payload: 2 })}>+</button>
+            <div>
+                <h2>Counting 1: {state.count}</h2>
+                <button onClick={() => dispatch({ type: 'decrement', payload: 5 })}>-</button>
+                <button onClick={() => dispatch({ type: 'increment', payload: 2 })}>+</button>
+            </div>
+            <div>
+                <h2>Counting 2: {state.counter2}</h2>
+                <button onClick={() => dispatch({ type: 'decrement2', payload: 5 })}>-</button>
+                <button onClick={() => dispatch({ type: 'increment2', payload: 2 })}>+</button>
+            </div>
         </div>
     );
 };
