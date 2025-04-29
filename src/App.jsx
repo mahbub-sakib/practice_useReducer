376eaeed1d923010452ef48a525b3d6b8b293426
mahbub-sakib <mahbub.sakib418@gmail.com>
2025-04-29 22:59:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Simple_counter from './components/Simple_counter'
+
+function App() {
+
+  return (
+    <div>
+      <Simple_counter></Simple_counter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Simple_counter.jsx
@@ -0,0 +1,28 @@
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
+}
+
+const Simple_counter = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <div>
+            <h2>Count: {state.count}</h2>
+            <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+            <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+        </div>
+    );
+};
+
+export default Simple_counter;
